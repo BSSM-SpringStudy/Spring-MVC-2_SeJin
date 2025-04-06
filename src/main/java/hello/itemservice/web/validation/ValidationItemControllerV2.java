374017f60e4b6a4c3dfe49a6b498b8e07c56a80a
허세진 <heosejin222@gmail.com>
2025-04-06 16:13:09 +0900

    Validator 분리1

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -25,6 +25,7 @@ public class ValidationItemControllerV2 {
 
     private static final Logger log = LoggerFactory.getLogger(ValidationItemControllerV2.class);
     private final ItemRepository itemRepository;
+    private final ItemValidator itemValidator;
 
     @GetMapping
     public String items(Model model) {
@@ -46,6 +47,27 @@ public class ValidationItemControllerV2 {
         return "validation/v2/addForm";
     }
     @PostMapping("/add")
+    public String addItemV5(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+
+     itemValidator.validate(item, bindingResult);
+
+        if (bindingResult.hasErrors()) {
+            log.info("errors={}", bindingResult);
+            return "validation/v2/addForm";
+        }
+        //성공 로직
+        Item savedItem = itemRepository.save(item); redirectAttributes.addAttribute("itemId", savedItem.getId()); redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+
+
+
+
+
+
+
+    //@PostMapping("/add")
     public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
 
         log.info("objectName={}",bindingResult.getObjectName());
@@ -193,4 +215,6 @@ public class ValidationItemControllerV2 {
     }
 
 }
+
+
 
